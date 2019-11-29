bcd8cbadb5d3271a160a3d7a29f3c5790e43d67e
Daniel Robertson <danielkr85@gmail.com>
2019-11-29 10:42:31 -0500

    superblock initial

---
+++ src/main/java/com/github/komododragon/superblocks/SuperBlocks.java
@@ -0,0 +1,8 @@
+package com.github.komododragon.superblocks;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SuperBlocks.MODID)
+public final class SuperBlocks {
+    public static final String MODID = "super_blocks";
+}
